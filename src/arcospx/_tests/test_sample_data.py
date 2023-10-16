9fb57a0a62192a24fdf2516f5e291d5b8a8171ac
leaBroe <lea.broennimann@students.unibe.ch>
2023-10-16 14:56:45 +0200

    Added tests

--- src/arcospx/_tests/test_sample_data.py
+++ src/arcospx/_tests/test_sample_data.py
@@ -1,7 +1,19 @@
-# from arcospx import make_sample_data
+import pytest
+from arcospx import make_sample_data
+import numpy as np
+from qtpy import QtCore
 
-# add your tests here...
 
+def test_make_sample_data():
+    """Test make_sample_data."""
+    data = make_sample_data()
 
-def test_something():
-    pass
+    # Check the shape of the numpy array
+    assert data[0][0].shape == (512, 512)
+
+    # Check if the first element of the tuple is a numpy array
+    assert isinstance(data[0][0], np.ndarray)
+
+    # Check if the second element of the tuple is a dictionary
+    assert isinstance(data[0][1], dict)
+
--- src/arcospx/_tests/test_widget.py
+++ src/arcospx/_tests/test_widget.py
@@ -1,59 +1,42 @@
 import numpy as np
-import unittest
-
+import pytest
+from qtpy import QtCore
 from arcospx import make_sample_data, remove_background, track_events
-import unittest
-from unittest.mock import Mock, patch
-from typing import Tuple
+import napari
+from skimage.io import imread
+from numpy.testing import assert_array_equal
+from napari.layers.image import Image
+
+def test_remove_background():
+    """Test background removal on a simple image."""
+    test_img = imread('src/arcospx/_tests/test_data/1_growing.tif')
+    test_img = Image(test_img)
+    true_img = imread('src/arcospx/_tests/test_data/1_growing_true.tif')
+    #test_img = np.where(test_img == 255, 0, 1)
+    removed_bg_img = remove_background(test_img.data, filter_type="gaussian", size_0=1, size_1=1, size_2=1)
+    assert_array_equal(removed_bg_img, true_img)
 
 
-class TestRemoveBackground(unittest.TestCase):
-
-    @patch('arcospx.remove_background')
-    def test_remove_background(self, mock_remove_background):
-        # Mocking the Image class
-        mock_image = Mock()
-        mock_image.data = make_sample_data()
-        mock_image.name = "test_image"
-
-        # Mocking the remove_background function to return a value
-        mock_remove_background.return_value = "removed_background_data"
-
-        result = remove_background(mock_image)
-
-        # Expected layer properties
-        expected_layer_properties = {
-            "name": "test_image background removed",
-            "metadata": {
-                "filter_type": "gaussian",
-                "size_0": 20,
-                "size_1": 5,
-                "size_2": 5,
-                "filename": "test_image",
-            },
-        }
-
-        # Asserting the results
-        self.assertEqual(result[0], "removed_background_data")
-        self.assertEqual(result[1], expected_layer_properties)
-        self.assertEqual(result[2], "image")
-
-        # Asserting the remove_background function was called with the right arguments
-        mock_remove_background.assert_called_once_with("image_data", "gaussian", (20, 5, 5))
-
-if __name__ == '__main__':
-    unittest.main()
-
+def test_track_events():
+    """
+    Test tracking on a simple image.
+    """
+    test_img = imread('test_data/test_data_track_events.tif')
+    test_img = Image(test_img)
+    true_img = imread('test_data/test_data_track_events_true.tif')
+    #test_img = np.where(test_img == 255, 0, 1)
+    tracked_img = track_events(test_img.data, threshold=300, eps=10, epsPrev=50, minClSz=50, nPrev=2)
+    assert_array_equal(tracked_img, true_img)
 
 # make_napari_viewer is a pytest fixture that returns a napari viewer object
 # capsys is a pytest fixture that captures stdout and stderr output streams
-# def test_example_q_widget(make_napari_viewer, capsys):
+# def test_remove_background(make_napari_viewer, capsys):
 #     # make viewer and add an image layer using our fixture
 #     viewer = make_napari_viewer()
 #     viewer.add_image(np.random.random((100, 100)))
 #
 #     # create our widget, passing in the viewer
-#     my_widget = ExampleQWidget(viewer)
+#     my_widget = remove_background(viewer)
 #
 #     # call our widget method
 #     my_widget._on_click()
