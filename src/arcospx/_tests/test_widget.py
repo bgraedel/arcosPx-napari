3505f994b9ccf16457785db0b6e752a3632ac842
leaBroe <lea.broennimann@students.unibe.ch>
2023-10-19 14:21:12 +0200

    added tests with removed background removal in track_events

--- src/arcospx/_tests/test_widget.py
+++ src/arcospx/_tests/test_widget.py
@@ -25,42 +25,9 @@ def test_track_events():
     Test tracking on a simple image.
     """
     viewer = napari.Viewer()
-    test_img = imread('test_data/test_track_events_bg_rm.tif')
+    test_img = imread('test_data/test_data_track_events.tif')
     viewer.add_image(test_img, name='test_img')
-    true_img = imread('test_data/test_track_events_true_bg_rm.tif')
+    true_img = imread('test_data/test_track_events_true.tif')
     tracked_img,_,_ = track_events(viewer.layers['test_img'], threshold=300, eps=10, epsPrev=50, minClSz=50, minSamples=2, nPrev=2)
     assert_array_equal(tracked_img, true_img)
 
-
-
-# make_napari_viewer is a pytest fixture that returns a napari viewer object
-# capsys is a pytest fixture that captures stdout and stderr output streams
-# def test_remove_background(make_napari_viewer, capsys):
-#     # make viewer and add an image layer using our fixture
-#     viewer = make_napari_viewer()
-#     viewer.add_image(np.random.random((100, 100)))
-#
-#     # create our widget, passing in the viewer
-#     my_widget = remove_background(viewer)
-#
-#     # call our widget method
-#     my_widget._on_click()
-#
-#     # read captured output and check that it's as we expected
-#     captured = capsys.readouterr()
-#     assert captured.out == "napari has 1 layers\n"
-#
-#
-# def test_example_magic_widget(make_napari_viewer, capsys):
-#     viewer = make_napari_viewer()
-#     layer = viewer.add_image(np.random.random((100, 100)))
-#
-#     # this time, our widget will be a MagicFactory or FunctionGui instance
-#     my_widget = example_magic_widget()
-#
-#     # if we "call" this object, it'll execute our function
-#     my_widget(viewer.layers[0])
-#
-#     # read captured output and check that it's as we expected
-#     captured = capsys.readouterr()
-#     assert captured.out == f"you have selected {layer}\n"
